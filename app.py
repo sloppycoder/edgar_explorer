725c9eab4623551cc6812d5fc8c3b418057b45fd
Li Lin <guru.lin@gmail.com>
2025-01-02 12:05:57 -0800

    created blank flask app

---
+++ app.py
@@ -0,0 +1,22 @@
+import json
+import logging
+import logging.config
+from pathlib import Path
+
+from dotenv import load_dotenv
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+load_dotenv()
+
+with open(Path(__file__).parent / "logger_config.json", "r") as f:
+    logging.config.dictConfig(json.load(f))
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,18 +6,4 @@ if [ -f ".env" ]; then
     source .env
 fi
 
-if [ "$DATABASE_URL" = "" ]; then
-    echo DATABASE_URL not set, aborting.
-    exit 1
-fi
-
-current_revision=$(aerich history | tail -n 1)
-latest_revision=$(aerich heads | tail -n 1)
-
-if [ "$current_revision" != "$latest_revision" ]; then
-  echo "Schema is not up to date. Applying latest revision..."
-  aerich upgrade
-else
-  echo "Schema is already up to date."
-fi
-/bin/sleep 100000
+gunicorn --workers 2 --bind 0.0.0.0 app:app
