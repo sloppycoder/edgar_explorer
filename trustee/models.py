3c4c6eda8be16d552cf89f20c19cf7015e077446
Li Lin <guru.lin@gmail.com>
2025-01-02 16:46:05 -0800

    add friendly name to model

--- trustee/models.py
+++ trustee/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class FilingIndexEntry(models.Model):
+    class Meta:
+        verbose_name = "SEC Filing in Master Index"
+        verbose_name_plural = "SEC Filings in Master Index"
+
     cik = models.CharField(max_length=10)
     company_name = models.CharField(max_length=150)
     form_type = models.CharField(max_length=20)
