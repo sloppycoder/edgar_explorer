1487cc8b56a4f028150c82363dcf4b1f3fab18eb
Li Lin <guru.lin@gmail.com>
2025-01-02 16:03:40 -0800

    create model, register with admin

--- trustee/admin.py
+++ trustee/admin.py
@@ -1,1 +1,23 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import FilingIndexEntry
+
+
+@admin.register(FilingIndexEntry)
+class FilingIndexEntryAdmin(admin.ModelAdmin):
+    list_display = (
+        "cik",
+        "company_name",
+        "form_type",
+        "accession_number",
+        "date_filed",
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- trustee/models.py
+++ trustee/models.py
@@ -1,1 +1,10 @@
-# Create your models here.
+from django.db import models
+
+
+class FilingIndexEntry(models.Model):
+    cik = models.CharField(max_length=10)
+    company_name = models.CharField(max_length=150)
+    form_type = models.CharField(max_length=20)
+    date_filed = models.CharField(max_length=10)
+    filename = models.CharField(max_length=100)
+    accession_number = models.CharField(max_length=20)
