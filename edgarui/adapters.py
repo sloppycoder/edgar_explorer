5ee4da796985303a6ffd937b3737aae90992c3c6
Li Lin <guru.lin@gmail.com>
2025-01-04 23:29:43 -0800

    added comment to stop pyright from complaining

--- edgarui/adapters.py
+++ edgarui/adapters.py
@@ -2,6 +2,6 @@ from allauth.account.adapter import DefaultAccountAdapter
 
 
 class NoSignupAdapter(DefaultAccountAdapter):
-    def is_open_for_signup(self, request):
+    def is_open_for_signup(self, request):  # pyright: ignore
         # Always return False to disable signup
         return False
