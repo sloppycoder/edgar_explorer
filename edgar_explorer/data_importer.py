5daedb6be68a8eeafb33f2e802458b6562781fcb
Li Lin <guru.lin@gmail.com>
2025-04-12 17:21:54 +0800

    added helper to load data from Big Query

---
+++ edgar_explorer/data_importer.py
@@ -0,0 +1,50 @@
+import json
+import os
+
+from google.cloud import bigquery
+
+from .models import Filing
+
+
+def load_filing_entries(batch_ids: list[str]) -> int:
+    table = os.environ.get("RESULT_TABLE", "edgar-ai.edgar2.extraction_result")
+    query = f"SELECT * FROM `{table}` WHERE batch_id IN UNNEST(@batch_ids) LIMIT 5000"
+
+    client = bigquery.Client()
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[bigquery.ArrayQueryParameter("batch_ids", "STRING", batch_ids)]
+    )
+    query_job = client.query(query, job_config=job_config)
+
+    n_count = 0
+    num_entities = 0
+    for row in query_job:
+        try:
+            info = json.loads(row["response"])
+            if row["extraction_type"] == "trustee_comp":
+                if info["compensation_info_present"]:
+                    num_entities = len(info["trustees"])
+            elif row["extraction_type"] == "fundmgr":
+                num_entities = len(info["managers"])
+            else:
+                continue
+        except json.JSONDecodeError:
+            pass
+
+        Filing.objects.create(
+            cik=row["cik"],
+            company_name="Some Company",
+            form_type="485BPOS",
+            date_filed=row["date_filed"],
+            accession_number=row["accession_number"],
+            chunks_used=row["selected_chunks"],
+            relevant_text=row["selected_text"],
+            num_entities=num_entities,
+            info=row["response"],
+            batch_id=row["batch_id"],
+        )
+        n_count += 1
+
+    print(f"Loaded {n_count} filings from {table}")
+
+    return n_count
---
+++ tests/test_data_loading.py
@@ -0,0 +1,11 @@
+import pytest  # noqa F401
+from edgar_explorer.data_importer import load_filing_entries
+from edgar_explorer.models import Filing
+
+
+@pytest.skip(reason="for local use only")
+def test_load_data_from_bigquery():
+    batch_id = "20250406182135-hap"
+    n_loaded = load_filing_entries([batch_id])
+    filings = Filing.objects.filter(batch_id=batch_id)  # pyright: ignore
+    assert len(filings) == n_loaded
