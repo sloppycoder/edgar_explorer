53d39f98b9a94186ce9877f467442908fe5571b5
Li Lin <guru.lin@gmail.com>
2025-04-22 00:39:04 +0800

    revert to load data from bigquery, remove firestore dependency

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -5,7 +5,7 @@ from edgar_explorer.models import Filing
 
 @pytest.mark.skip(reason="for local use only")
 def test_load_data_from_bigquery():
-    batch_id = "20250419094434-acp"
+    batch_id = "20250422002102-ynv"
     n_loaded = load_filing_entries([batch_id])
     filings = Filing.objects.filter(batch_id=batch_id)  # pyright: ignore
     assert len(filings) == n_loaded
